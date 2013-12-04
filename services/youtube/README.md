795ac0312f4a0013e594045a32468f5f80529806
John Smith <assaf@youappi.com>
2013-12-04 18:38:42 +0200

    added youtube module

---
+++ services/youtube/README.md
@@ -0,0 +1,26 @@
+# youtube [![Build Status](https://secure.travis-ci.org//youtube.png?branch=master)](http://travis-ci.org//youtube)
+
+The best module ever.
+
+## Getting Started
+Install the module with: `npm install youtube`
+
+```javascript
+var youtube = require('youtube');
+youtube.awesome(); // "awesome"
+```
+
+## Documentation
+_(Coming soon)_
+
+## Examples
+_(Coming soon)_
+
+## Contributing
+In lieu of a formal styleguide, take care to maintain the existing coding style. Add unit tests for any new or changed functionality. Lint and test your code using [Grunt](http://gruntjs.com/).
+
+## Release History
+_(Nothing yet)_
+
+## License
+Copyright (c) 2013 . Licensed under the MIT license.
---
+++ services/youtube/test/youtube_test.js
@@ -0,0 +1,44 @@
+'use strict';
+
+var youtube = require('../lib/youtube.js');
+
+/*
+  ======== A Handy Little Nodeunit Reference ========
+  https://github.com/caolan/nodeunit
+
+  Test methods:
+    test.expect(numAssertions)
+    test.done()
+  Test assertions:
+    test.ok(value, [message])
+    test.equal(actual, expected, [message])
+    test.notEqual(actual, expected, [message])
+    test.deepEqual(actual, expected, [message])
+    test.notDeepEqual(actual, expected, [message])
+    test.strictEqual(actual, expected, [message])
+    test.notStrictEqual(actual, expected, [message])
+    test.throws(block, [error], [message])
+    test.doesNotThrow(block, [error], [message])
+    test.ifError(value)
+*/
+
+var config = {
+    apiKey:'AIzaSyBL6PS3qcjaI4KSCrysejNsFHNQkHtXShs'
+};
+
+exports.youtube = {
+  setUp: function(done) {
+    // setup here
+    done();
+  },
+  'no args': function(test) {
+    test.expect(2);
+    // tests here
+      var service = youtube(config);
+      service.search('CK', function(err, results){
+          test.equal(err, null);
+          test.equal(results.items.length, 10);
+          test.done();
+      });
+  }
+};
